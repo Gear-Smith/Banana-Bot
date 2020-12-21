e1102be6a262aea9a534166792fb6513414d9c89
Raymond Huff <rehuff83@gmail.com>
2020-12-20 21:37:13 -0500

    Fixed "join" and "summon" commands
    Bot not joins authors channel or moves to authors channel when summoned.

--- cogs/audio.py
+++ cogs/audio.py
@@ -5,7 +5,7 @@ import youtube_dl
 
 class VoiceEntry:
     def __init__(self, ctx, player):
-        self.requrester = ctx.author
+        self.requester = ctx.author
         self.channel = ctx.author
         self.player = player
 
@@ -14,12 +14,12 @@ class VoiceEntry:
         duration = self.player.duration
         if duration:
             fmt = fmt + ' [length: {0[0]}m {0[1]}s]'.format(divmod(duration, 60))
-        return fmt.format(self.player, self.requrester)
+        return fmt.format(self.player, self.requester)
 
 async def create_voice_client(self, ctx):
     
      await ctx.send("Attempting to join: " + str(ctx.author.voice.channel))
-     voice = await discord.VoiceChannel.connect(ctx.author.voice.channel)
+     self.voice = await discord.VoiceChannel.connect(ctx.author.voice.channel)
      
           
 
@@ -28,11 +28,11 @@ class AudioCog(commands.Cog):
 
     def __init__(self,bot):
         self.bot = bot
-        self.voice_states = {}
+        self.voice_states = {None}
+        self.voice = None
         
     def get_voice_state(self):
         self.voice_states = self.bot.voice_clients
-        print(self.voice_states)
         return self.voice_states
         
     @commands.command(pass_context=True, no_pm=True)
@@ -53,5 +53,33 @@ class AudioCog(commands.Cog):
             await ctx.send('**`Ready to play audio in:`**' + str(ctx.author.voice.channel))
             print(ctx.author.voice.channel)
             
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def summon(self, ctx):
+        """Summons the bot to join your voice channel."""
+        self.get_voice_state()
+        
+        summoned_channel = ctx.author.voice.channel
+        
+        if summoned_channel is None:
+            await ctx.send('You are not in a voice channel.')
+            return False
+        
+        if self.voice_states is None:
+            try:
+                await create_voice_client(self, ctx)
+            except Exception as e:
+                await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+        
+        else:
+            try:
+                await self.voice.move_to(ctx.author.voice.channel)
+                await ctx.send('**`Moved to audio channel:`**' + str(ctx.author.voice.channel))
+            except Exception as e:
+                await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+
+        return True
+
+
 def setup(bot):
     bot.add_cog(AudioCog(bot))
