de8c5f7cc18ef85bb95d69cb7ef1e3542e2a2b3a
Raymond Huff <rehuff83@gmail.com>
2022-04-02 00:37:33 -0400

    Stable update
    Added persistence with registered streamer data

--- utilities.py
+++ utilities.py
@@ -1,19 +1,7 @@
 import configparser
+import ast
+from pprint import pprint
 from xml.etree.ElementTree import tostring
-
-class Utilities():
-    """Functions for Wolf to interatct with channels and messages."""
-    
-    def __init__(self):
-        pass
-
-    def strip_username(self):
-        """Strip username from twitch URL. Returns username."""
-        pass
-
-    def catalog_live_channels(self):
-        """Adds streamcord pings to a live channel list. Returns the current list"""
-        pass
 
 Config = configparser.ConfigParser()
 Config.read('config.ini')
@@ -25,6 +13,7 @@ class configs():
         self.bot_token = Config.get('Discord', 'DISCORD_BOT_TOKEN')
         self.client_id = Config.get('Twitch', 'CLIENT_ID')
         self.client_secret = Config.get('Twitch', 'CLIENT_SECRET')
+        self.discord_live_channel_id = Config.get('Discord', 'discord_live_channel_id')
 
     def setProperty(self, option, value, discord_name):
         
@@ -34,6 +23,13 @@ class configs():
         with open('config.ini', 'w') as configfile:
             Config.write(configfile)
             Config.read('config.ini')
+    
+    def get_streamer_list(self):
+        output = {}
+        for key in Config['Streamers']:
+            output[key] = ast.literal_eval(Config.get('Streamers', key, raw=True))
+        
+        return output
 
     def get_bot_token(self):
         return self.bot_token
@@ -43,3 +39,21 @@ class configs():
 
     def get_client_secret(self):
         return self.client_secret
+    
+    def get_live_channel_id(self):
+        return self.discord_live_channel_id
+
+
+class Utilities():
+    """Functions for bot to interatct with channels and messages."""
+    
+    def __init__(self):
+        pass
+
+    def strip_username(self):
+        """Strip username from twitch URL. Returns username."""
+        pass
+
+    def catalog_live_channels(self):
+        """Adds streamcord pings to a live channel list. Returns the current list"""
+        pass
